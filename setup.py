cfb01bcaae0eadb678ad6bbed62d4e6d80c3cf67
Adomas <107749872+0xleft@users.noreply.github.com>
2023-07-22 22:57:03 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='discordhook',
+    version='1.0',
+    description='It just makes ur life easy so why not use it?',
+    author='0xleft',
+    url='https://pageup.lt/',
+    packages=['.'],
+    long_description="Amazing",
+    license='Apache License 2.0',
+)
