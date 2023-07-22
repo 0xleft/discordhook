d5a7d97e6aaad337fef2128570255f63cb553629
Adomas <107749872+0xleft@users.noreply.github.com>
2023-07-22 23:08:13 +0200

    bam

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(
-    name='discordhook',
+    name='weehok',
     version='1.0',
     description='It just makes ur life easy so why not use it?',
     author='0xleft',
     url='https://pageup.lt/',
-    packages=['.'],
+    packages=['weehok'],
     long_description="Amazing",
     license='Apache License 2.0',
 )
